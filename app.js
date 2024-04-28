94478dfb7c17979fb357e0e688080ce49225b20a
Dmitry_Hrynchyk <grinchikdmitry@gmail.com>
2024-04-28 15:23:38 +0200

    Created main web-app

---
+++ app.js
@@ -0,0 +1,5 @@
+//Import all components
+// tv.js will only load what is needed
+
+//$tv.setConfig({ renderAll: false });
+// $tv.import({ define: 'site-datahandler', file: '/src/components/additionals/DataHandler'});
