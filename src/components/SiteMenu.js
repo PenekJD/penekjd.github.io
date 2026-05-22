b6e51463f5c281e854a5b843f0131aa1156fe9db
Dmitry_Grinchik <grinchikdmitry@gmail.com>
2026-05-22 10:40:29 +0200

    Fix check_word element styles

--- src/components/SiteMenu.js
+++ src/components/SiteMenu.js
@@ -10,7 +10,7 @@ class SiteMenu extends TvAlpineHTMLElement {
                 Languager
             </h2>
         </a>
-        <code>v 2.0.6</code>
+        <code>v 2.0.7</code>
     </section>
     <nav>
         <ul>
