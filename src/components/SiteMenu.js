006fcc6a67b569910650ab8401419585a1c2b721
Dmitry_Grinchik <grinchikdmitry@gmail.com>
2026-05-27 20:35:56 +0200

    Fix mainpage meta description

--- src/components/SiteMenu.js
+++ src/components/SiteMenu.js
@@ -10,7 +10,7 @@ class SiteMenu extends TvAlpineHTMLElement {
                 Languager
             </h2>
         </a>
-        <code>v 2.1.1</code>
+        <code>v 2.1.2</code>
     </section>
     <nav role="navigation">
         <ul>
