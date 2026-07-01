d247cbcb53de0c6f54da9cce3a9acab47887fb17
DmitryiH <grinchikdmitry@gmail.com>
2026-07-01 02:11:07 +0200

    Fix stylesheet typo

--- src/components/SiteMenu.js
+++ src/components/SiteMenu.js
@@ -10,7 +10,7 @@ class SiteMenu extends TvAlpineHTMLElement {
                 Languager
             </h2>
         </a>
-        <code>v 2.1.7</code>
+        <code>v 2.1.8</code>
     </section>
     <nav role="navigation">
         <ul>
